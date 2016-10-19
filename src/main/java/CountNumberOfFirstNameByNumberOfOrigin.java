08b7943e98e28f370aba4ca763b7e713085def11
Félicien Catherin <felicien.catherin@gmail.com>
2016-10-19 18:04:23 +0200

    CountNumberOfFirstNameByNumberOfOrigin OK ! Creation de Proportion MaleFemale

--- src/main/java/CountNumberOfFirstNameByNumberOfOrigin.java
+++ src/main/java/CountNumberOfFirstNameByNumberOfOrigin.java
@@ -1,6 +1,59 @@
 /**
  * Created by felicien on 19/10/16.
  */
-public class CountNumberOfFirstNameByNumberOfOrigin {
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
 
+import java.io.IOException;
+import java.util.Iterator;
+
+public class CountNumberOfFirstNameByNumberOfOrigin {
+    private static class Map extends MapReduceBase implements Mapper<LongWritable, Text, IntWritable, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        public void map(LongWritable key, Text value, OutputCollector<IntWritable, IntWritable> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            //Sépare la ligne selon les ;
+            String[] lineSplit = line.split(";");
+            //Sépare les origines avec les virgules
+            String[] originSplit = lineSplit[2].split(",");
+
+            if (originSplit.length==0) {
+                output.collect(new IntWritable(0), one);
+            } else {
+                output.collect(new IntWritable(originSplit.length), one);
+            }
+        }
+    }
+
+    private static class Reduce extends MapReduceBase implements Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+        public void reduce(IntWritable nbOfOrigins, Iterator<IntWritable> iterator, OutputCollector<IntWritable, IntWritable> outputCollector, Reporter reporter) throws IOException {
+            int sum = 0;
+            while (iterator.hasNext()) {
+                sum += iterator.next().get();
+            }
+            outputCollector.collect(nbOfOrigins, new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        JobConf conf = new JobConf(CountNumberOfFirstNameByNumberOfOrigin.class);
+        conf.setJobName("CountNumberOfFirstNameByNumberOfOrigin");
+        conf.setOutputKeyClass(IntWritable.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setMapperClass(Map.class);
+        conf.setCombinerClass(Reduce.class);
+        conf.setReducerClass(Reduce.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+        JobClient.runJob(conf);
+    }
 }
---
+++ src/main/java/ProportionMaleFemale.java
@@ -0,0 +1,5 @@
+/**
+ * Created by felicien on 19/10/16.
+ */
+public class ProportionMaleFemale {
+}

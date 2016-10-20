97cde33ada94c73f092b24cf7e0d751de30a4d92
Félicien Catherin <felicien.catherin@gmail.com>
2016-10-20 16:07:05 +0200

    ProportionMaleFemale compte le nombre d'homme et de femme

--- src/main/java/ProportionMaleFemale.java
+++ src/main/java/ProportionMaleFemale.java
@@ -1,6 +1,65 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+import java.io.IOException;
+import java.util.Iterator;
+
 /**
  * Created by felicien on 19/10/16.
  */
 public class ProportionMaleFemale {
+
+    private static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            //Récupère la line à traiter
+            String[] lineSplit = line.split(";");
+            //Sépare la ligne selon les ;
+            String[] originSplit = lineSplit[1].split(",");
+            //Sépare les origines avec les virgules
+            for (String s : originSplit) {
+                word.set(s.trim());
+                output.collect(word, one);
+                //Ajout d'un 1 dans le dictionnaire pour le mot correspondant
+            }
+        }
+    }
+
+    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            int sum = 0;
+            while (values.hasNext()) {
+                //Compte le nombre de 1 de la value
+                sum += values.next().get();
+            }
+            output.collect(key, new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        JobConf conf = new JobConf(CountFirstNameByOrigin.class);
+        conf.setJobName("ProportionMaleFemale");
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setMapperClass(Map.class);
+        conf.setCombinerClass(Reduce.class);
+
+        conf.setReducerClass(Reduce.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+        JobClient.runJob(conf);
+    }
 }
 
--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@
 mvn package
 
 scp target/TP2-0.1.jar hadoop:TP2.jar
-ssh hadoop "hdfs dfs -rm -r out || true && hadoop jar TP2.jar CountNumberOfFirstNameByNumberOfOrigin /res/prenoms.csv out && rm -rf out || true && hdfs dfs -get out"
+#ssh hadoop "hdfs dfs -rm -r out || true && hadoop jar TP2.jar CountNumberOfFirstNameByNumberOfOrigin /res/prenoms.csv out && rm -rf out || true && hdfs dfs -get out"
 #ssh hadoop "hdfs dfs -rm -r out || true && hadoop jar TP2.jar CountFirstNameByOrigin /res/prenoms.csv out && rm -rf out || true && hdfs dfs -get out"
-#ssh hadoop "hdfs dfs -rm -r out || true && hadoop jar TP2.jar ProportionMaleFemale /res/prenoms.csv out && rm -rf out || true && hdfs dfs -get out"
+ssh hadoop "hdfs dfs -rm -r out || true && hadoop jar TP2.jar ProportionMaleFemale /res/prenoms.csv out && rm -rf out || true && hdfs dfs -get out"
 
 
 
@@ -13,4 +13,4 @@ rm -rf output
 scp -r hadoop:out output
 
 
-cat output/part-00000
+cat output/part-00000 |column -t
